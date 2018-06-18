0b688b97cbd9cbec28cd6cb788eea2d1c46737a5
Chris Spencer <cvspencer@gmail.com>
2018-06-18 13:59:25 +0900

    L53 -> Completed class

--- src/tut1/api/threads/scheduling/L53.java
+++ src/tut1/api/threads/scheduling/L53.java
@@ -7,5 +7,28 @@ package tut1.api.threads.scheduling;
 
 // NOTE: Can only run Runnables
 
+import tuts.common.NamedThreadsFactory;
+import tuts.common.ScheduledTaskB;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public class L53 {
+
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yy HH:mm:ss.SSS");
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Current time: " + dateFormatter.format(new Date()));
+
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(3, new NamedThreadsFactory());
+        ScheduledFuture<?> sf1 = exec.scheduleAtFixedRate(new ScheduledTaskB(1000), 4,2, TimeUnit.SECONDS);
+        ScheduledFuture<?> sf2 = exec.scheduleAtFixedRate(new ScheduledTaskB(3000), 4,2, TimeUnit.SECONDS);
+
+        TimeUnit.MILLISECONDS.sleep(10000);
+        exec.shutdown();
+    }
 }
